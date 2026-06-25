c322dc493e0c6ef0c6f969c9702e800e31d4d604
sonhocdot <tranthaison9303@gmail.com>
2026-06-25 10:09:17 +0700

    feat(backend): Hoàn thành chức năng auth

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\DemoController;        
+use App\Http\Controllers\Api\DemoController;
+use App\Http\Controllers\Api\AuthController;        
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/demos', [DemoController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Ví dụ một Route kiểm tra xem Token có hợp lệ và lấy thông tin User hiện tại
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
